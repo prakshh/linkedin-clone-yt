6fcf0dcccbf52504c3ca45ac89ec704118e28645
“prakshh” <prakashdas18@gmail.com>
2021-12-19 10:28:01 +0530

    header sections

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+
+function Header() {
+    return (
+        <div className="header">
+
+            <div className="header__left">
+                <img 
+                    src="./assets/images/linkedin-logo.png"
+                    alt="" 
+                />
+                <div className="header__search">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+            </div>
+
+            <div className="header__right">
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
