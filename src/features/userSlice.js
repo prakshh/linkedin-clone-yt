ca1f93c857b899d76b8e5db4d9b2d491d20c1d85
“prakshh” <prakashdas18@gmail.com>
2021-12-22 12:04:31 +0530

    userSlice.js creation

---
+++ src/features/userSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// const initialState = {
+//   value: 0,
+//   status: 'idle',
+// };
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: null,
+  },
+
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = userSlice.actions;
+
+export const selectUser = (state) => state.user.value; // to pull from data layer 
+
+export default userSlice.reducer;
