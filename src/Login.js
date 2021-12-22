6c5262ce1ac406154c4a92ff410523061e298768
“prakshh” <prakashdas18@gmail.com>
2021-12-22 14:15:15 +0530

    Login page designed

--- src/Login.js
+++ src/Login.js
@@ -2,9 +2,26 @@ import React from 'react'
 import './Login.css'
 
 function Login() {
+    const loginToApp = () => {
+
+    };
+    const register = () => {
+
+    };
     return (
         <div className='login'>
-            <h1>You are not logged in</h1>
+            <img src="./assets/images/linkedin-form-logo.jpg" alt="" />
+            <form>
+                <input placeholder="full name (required if registering)" type="text" />
+                <input placeholder="Profile pic URL (optional)" type="text" />
+                <input placeholder="Email" type="email" />
+                <input placeholder="Password" type="password" />
+                <button type='submit' onClick={loginToApp}>Sign in</button>
+            </form>
+                <p>Not a member? 
+                    <span className='login__register' onClick={register}> Register Now</span>
+                </p>
+            {/* </form> */}
         </div>
     )
 }
