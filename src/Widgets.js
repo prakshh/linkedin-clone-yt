5eb3a1900da58d3ea92757eba00252922bb6948c
“prakshh” <prakashdas18@gmail.com>
2021-12-23 15:11:44 +0530

    Widget Section Built

---
+++ src/Widgets.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Widgets.css'
+import InfoIcon from '@material-ui/icons/Info'; 
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+
+function Widgets() {
+
+    const newsArticle = (heading, subtitle) => (
+        <div className="widgets__article">
+            <div className="widgets__articleLeft">
+                <FiberManualRecordIcon />
+            </div>
+            <div className="widgets__articleRight">
+                <h4>{heading}</h4>
+                <p>{subtitle}</p>
+            </div>
+        </div>
+    )
+
+    return (
+        <div className="widgets">
+            <div className="widgets__header">
+                <h2>LinkedIn News</h2>
+                <InfoIcon />
+            </div>
+            {newsArticle("SpaceX is back", "Top news : 9099 readers")}
+            {newsArticle("Solar Planet: UK updates", "Top news - 886 reader")}
+            {newsArticle("Bitcoin Breaks $22K", "Crypto - 8000 reader")}
+            {newsArticle("Google Workspace Emerges", "Get Ready - 120000 reader")}
+            {newsArticle("Tesla hits new highs", "Cars & auto - 300 reader")}           
+        </div>
+    )
+}
+
+export default Widgets 
